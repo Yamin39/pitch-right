1cae2c368f25b87b31170bd44b632d95d8d58404
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-04-19 21:51:46 +0600

    added navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,44 @@
 import { CgMenuRightAlt } from "react-icons/cg";
 import { IoClose } from "react-icons/io5";
-import { NavLink } from "react-router-dom";
-import logo from "../../../../assets/logo-full-black.png";
+import logo from "../../assets/images/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   const links = (
     <>
-      <li className="text-[#fe4117]">
-        <NavLink to="/host">Host Ads for us</NavLink>
+      <li className="nav-link">
+        <a href="#">About us</a>
       </li>
-      <li>
-        <NavLink to="/app">Graphic Workshop</NavLink>
+      <li className="nav-link">
+        <a href="#">Services</a>
       </li>
-      <li>
-        <NavLink to="/app">Web Design</NavLink>
+      <li className="nav-link">
+        <a href="#">Why Choose us</a>
+      </li>
+      <li className="nav-link">
+        <a href="#">Ask Away</a>
+      </li>
+      <li className="nav-link">
+        <a href="#">Testimonials</a>
       </li>
     </>
   );
   return (
-    <nav className="pt-4 lg:pt-8 pb-4 sm:pb-8 bg-[#F5F5F5]">
+    <nav className="pt-4 lg:pt-8 pb-4 sm:pb-8">
       <div className="flex justify-between items-center">
         <div className="sm:w-auto">
           <a href="/">
-            <img className="w-[11.625rem]" src={logo} alt="Graphic Digital" />
+            <img className="max-w-[16rem]" src={logo} alt="Graphic Digital" />
           </a>
         </div>
-        <ul className="hidden md:flex gap-7 justify-center items-center text-lg text-[#1b2426] font-light font-poppins">{links}</ul>
-        <div className="md:hidden">
+        <div className="hidden lg:flex gap-7 justify-center items-center">{links}</div>
+        <div className="lg:hidden">
           <div className="drawer drawer-end">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
               {/* Page content here */}
-              <label htmlFor="my-drawer-4" className="drawer-button btn">
-                <CgMenuRightAlt className="text-2xl" />
+              <label htmlFor="my-drawer-4" className="drawer-button btn btn-ghost">
+                <CgMenuRightAlt className="text-3xl" />
               </label>
             </div>
             <div className="drawer-side">
@@ -43,7 +49,7 @@ const Navbar = () => {
                   <label htmlFor="my-drawer-4" aria-label="close sidebar" className="btn btn-circle btn-error btn-sm">
                     <IoClose className="text-white text-lg" />
                   </label>
-                </div>{" "}
+                </div>
                 {/* Sidebar content here */}
                 {links}
               </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="max-w-[1201px] w-11/12 mx-auto font-hanken">
       <Navbar></Navbar>
     </div>
   );
